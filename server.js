e0548912bb6486167edca90185678e598117f95a
brettpenzer123 <brettpenzer123@live.co.uk>
2021-05-08 17:29:43 +0100

    Revert "Update server.js"
    This reverts commit 0078f038394d221aa454ea34d325d2a396bfe0e4.

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
+'use strict';
+
 const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
+const { Server } = require('ws');
 
-const port = 6969;
-const server = http.createServer(express);
-const wss = new WebSocket.Server({ server })
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
 
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-wss.on('open', function open() {
-  console.log('connected');
-  ws.send(Date.now());
-});
+const wss = new Server({ server });
 
-wss.on('close', function close() {
-  console.log('disconnected');
-});
-
-server.listen(port, function() {
-  console.log(`Server is listening on ${port}!`)
-})
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('message', () => {wss.clients.forEach(function each(client){
+      if (client.readyState === Server.OPEN) {
         client.send(data);
       }
-    });
+	  });
 	console.log(data);
-  });
+	});
 });
